02a376e634534b71bbd4d61f5353776cf0009cf4
ShanteTorres <kennedy2110@gmail.com>
2022-01-06 23:02:38 -0500

    commit tree 5a59cddf9e25fd8fa1f6ee55d30f689b884273ae

--- src/index.js
+++ src/index.js
@@ -12,4 +12,74 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+  toyFormContainer.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postToy(e.target.name.value, e.target.image.value)
+  })
+})
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      data.map(t => renderToy(t))
+      //console.log(data)
+    })
+}
+
+function renderToy(toy) {
+  const toyCard = `<div class="card">
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p>${toy.likes}Likes</p>
+  <button class="like-btn">Like ❤️</button>
+</div>`
+
+  const toyBox = document.getElementById('toy-collection')
+  toyBox.innerHTML += toyCard
+
+}
+
+function postToy(name, url) {
+  fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "name": name,
+          "image": url,
+          "likes": 0
+        })
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+        console.log(data)
+      })
+  }
+
+  function likes(e) {
+    e.preventDefault()
+    let more = parseInt(e.target.previousElementSibling.innerText) + 1
+  
+    fetch(`http://localhost:3000/toys/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+  
+        },
+        body: JSON.stringify({
+          "likes": more
+        })
+      })
+      .then(res => res.json())
+      .then((like_obj => {
+        e.target.previousElementSibling.innerText = `${more} likes`;
+      }))
+  }
